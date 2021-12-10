2b96b0dc05cc3891c78530da40ab8bb5acfe0c4c
USER <mubashirnadeem952@gmail.com>
2021-12-10 15:02:33 +0500

    commit

--- ToasterMessage2/src/main/java/com/example/toastermessage2/CalculatorActivity.java
+++ ToasterMessage2/src/main/java/com/example/toastermessage2/CalculatorActivity.java
@@ -23,7 +23,7 @@ public class CalculatorActivity extends AppCompatActivity {
         mEditText2 = findViewById(R.id.edittext_number_2);
        // mTextViewResult = findViewById(R.id.textview_result);
 
-        Button mButtonAdd = findViewById(R.id.button);
+        Button mButtonAdd = (Button) findViewById(R.id.button);
 
 
 
