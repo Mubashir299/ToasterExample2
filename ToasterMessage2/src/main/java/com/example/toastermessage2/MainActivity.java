776564bd8ccefa0d617cb11d29833541741478fa
USER <mubashirnadeem952@gmail.com>
2021-12-10 11:10:50 +0500

    ffd

--- ToasterMessage2/src/main/java/com/example/toastermessage2/MainActivity.java
+++ ToasterMessage2/src/main/java/com/example/toastermessage2/MainActivity.java
@@ -21,8 +21,11 @@ public class MainActivity extends AppCompatActivity {
         mEditText1 = findViewById(R.id.edittext_number_1);
         mEditText2 = findViewById(R.id.edittext_number_2);
         mTextViewResult = findViewById(R.id.textview_result);
+/*
         Button mButtonAdd = findViewById(R.id.button_add);
+*/
 
+/*
         mButtonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,7 +48,28 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+*/
 
 
+    }
+
+    public void Cal(View view) {
+        if (mEditText1.getText().toString().length() == 0) {
+            mEditText1.setText("0");
+        }
+
+        if (mEditText2.getText().toString().length() == 0) {
+            mEditText2.setText("0");
+        }
+
+
+        int num1 = Integer.parseInt(mEditText1.getText().toString());
+        int num2 = Integer.parseInt(mEditText2.getText().toString());
+
+        int sum = num1 + num2;
+
+        mTextViewResult.setText(String.valueOf(sum));
+        ToasterMessage2.display(MainActivity.this, sum);
+        finish();
     }
 }
