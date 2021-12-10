824d19bbb83322540051e28952ddd6b3d4e76fb7
USER <mubashirnadeem952@gmail.com>
2021-12-10 12:28:17 +0500

    com

--- ToasterMessage2/src/main/java/com/example/toastermessage2/ToasterMessage2.java
+++ ToasterMessage2/src/main/java/com/example/toastermessage2/ToasterMessage2.java
@@ -13,9 +13,13 @@ public class ToasterMessage2 {
     public static void calculator(Context c) {
         c.startActivity(new Intent(c, CalculatorActivity.class));
 
+
     }
 
     public static void display(Context c, int result) {
         s(c, "result is:" + result);
+        Toast.makeText(c, "result is :"+result, Toast.LENGTH_SHORT).show();
     }
+
+
 }
