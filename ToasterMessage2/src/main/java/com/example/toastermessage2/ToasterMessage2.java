6217335c5f023678ec1d0f405d04bb750198b251
USER <mubashirnadeem952@gmail.com>
2021-12-10 10:08:04 +0500

    giT

--- ToasterMessage2/src/main/java/com/example/toastermessage2/ToasterMessage2.java
+++ ToasterMessage2/src/main/java/com/example/toastermessage2/ToasterMessage2.java
@@ -1,11 +1,21 @@
 package com.example.toastermessage2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 public class ToasterMessage2 {
     public static void s(Context c, String message) {
 
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void calculator(Context c) {
+        c.startActivity(new Intent(c, MainActivity.class));
+
+    }
+
+    public static void display(Context c, int result) {
+        s(c, "result is:" + result);
     }
 }
